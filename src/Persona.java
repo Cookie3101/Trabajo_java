499d541b30a048e18488a0c687d2b2020acaa971
dahiana <dayabenitez2@gmail.com>
2024-05-07 07:30:33 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por Dahiana

--- src/Persona.java
+++ src/Persona.java
@@ -2,4 +2,30 @@ public class Persona {
     private String Nombre;
     private int Edad;
     private String FechaNacimiento;
+
+    
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public String getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
+    }
 }
+
