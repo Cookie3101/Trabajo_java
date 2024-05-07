a87d03142d5a55f1a187bea114f67676fc850f52
dahiana <dayabenitez2@gmail.com>
2024-05-07 12:08:25 -0500

    Herencia y Polimorfismo - Implementación de Subclases por Dahiana

--- src/Empleado.java
+++ src/Empleado.java
@@ -2,11 +2,13 @@ public class Empleado implements Trabajador{
     private String Nombre;
     private double Salario;
 
+    public Empleado(String nombre, double salario) {
+    }
+
     @Override
     public void realizarTarea() {
         System.out.println("Realizando tarea laboral");
     }
-
 
 
 }
